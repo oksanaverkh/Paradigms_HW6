0b24531caf4036f8c373f289267d2fa67100a1c6
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-09-19 18:52:29 +0300

    Final commit

--- Task1.py
+++ Task1.py
@@ -5,35 +5,25 @@
 # На выходе - индекс элемента или -1, в случае если искомого элемента нет в массиве.
 
 import random
-
-
-def centerFind(arr):
-    if len(arr) % 2 == 0:
-        return len(arr)//2-1
-    else:
-        return len(arr)//2
-
-
-def numberFind(arr, num):
-    count = 0
-    for i in range(len(arr)):
-        if arr[i] == num:
-            count += 1
-            break
-    if count == 0:
-        return -1
-    else:
-        return i
+from math import ceil
 
 
 def binarySort(arr, num):
-    center = centerFind(arr)
-    if num > arr[center]:
-        return numberFind(arr[center:], num) + center
-    elif num == arr[center]:
+    center = len(arr) // 2
+    left = 0
+    right = len(arr) - 1
+
+    while num != arr[center] and left <= right:
+        if num > arr[center]:
+            left = center + 1
+        else:
+            right = center - 1
+        center = (left+right)//2
+
+    if left <= right:
         return center
     else:
-        return numberFind(arr[:center], num)
+        return -1
 
 
 def main():
